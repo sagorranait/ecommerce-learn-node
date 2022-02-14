8cf83bd47dacec89c3eb487a874c5167d7e7901a
SAGOR RANA <60606778+sagorranait@users.noreply.github.com>
2022-02-14 23:09:44 +0600

    update

--- models/product.js
+++ models/product.js
@@ -2,11 +2,6 @@ const products = [];
 
 module.exports = class Products{
   constructor(id, title, imageUrl, description, price){
-    // this.id = Math.random();
-    // this.title = title;
-    // this.imageUrl = imageUrl;
-    // this.description = description;
-    // this.price = price;
     this.storeProduct = {id, title, imageUrl, description, price}
   }
 
@@ -19,11 +14,7 @@ module.exports = class Products{
   }
 
   static productDetalisById(id){
-    const product = products.find(p => {
-      console.log(p);
-      return p.id === id;
-    });
-    console.log(product);
-    return product;
+    const product = [products];
+    return product.find(p => p.id === id);
   }
 }
